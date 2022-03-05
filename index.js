ada6ad94bd4ea7cacb156ef1d678cca30945c3b8
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-03-05 12:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,5 +80,20 @@ app.get("/", function (req, res) {
 });
 
 
-app.listen(3000);
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
+// error handler middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || "Internal Server Error",
+    },
+  });
+});
+
+app.listen(process.env.PORT || 3000);
