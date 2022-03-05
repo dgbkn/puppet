db64779d8f5e7cc1037843dd4643e66d33a41331
dgbkn <anandrambkn@gmail.com>
2022-03-05 12:01:01 +0530

    asada

--- index.js
+++ index.js
@@ -12,57 +12,48 @@ const puppeteer = require('puppeteer');
 
 app.use(cors());
 
-
-// const main = async () => {
-//     const browser = await puppeteer.launch({headless: false,args: [
-//         '--no-sandbox',
-//         '--disable-setuid-sandbox',
-//       ],});
-//         const page = await browser.newPage();
-//         await page.goto('https://google.com');
-
-//         const pdf = await page.pdf();
-//         return pdf;
-//      }
-
-
-const cloudloginandrenew = async () => {
+async function startBrowser() {
     const browser = await puppeteer.launch({
-       args: [
+        args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
         ],
     });
-
     const page = await browser.newPage();
-    await page.setViewport({width: 1200, height: 720})
-    await page.goto('https://ccp.cloudaccess.net/index.php?rp=/login', { waitUntil: 'networkidle0' });
-    await page.waitForNavigation();
-    await page.type('#inputEmail', "martmast60@gmail.com");
-    await page.type('#inputPassword', "@Anu123456");
-
-  // click and wait for navigation
-  await Promise.all([
-    page.click('#login'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  ]);
-
-    const pdf = await page.pdf();
-    return pdf;
+    return { browser, page };
 }
+
+async function closeBrowser(browser) {
+    return browser.close();
+}
+
+
+async function cloudloginandrenew() {
+    const { browser, page } = await startBrowser();
+    page.setViewport({ width: 1366, height: 768 });
+    await page.goto("https://ccp.cloudaccess.net/index.php?rp=/login");
+    await page.click('#inputEmail');
+    await page.keyboard.type('martmast60@gmail.com');
+    await page.click('#inputPassword');
+    await page.keyboard.type('@Anu123456');
+    await page.click('#login');
+    await page.waitForNavigation();
+    const ss = await page.screenshot();
+    return ss;
+}
+
 
 
 app.get('/cloudlogin', async function (req, res) {
     const pdf = await cloudloginandrenew();
-    res.contentType("application/pdf");
-
+    // res.contentType("application/pdf");
     res.send(pdf);
 });
 
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/index.htm"));
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 
