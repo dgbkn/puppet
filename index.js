8b4e51a4e01e7c3d8eab3472fac0b02e69912736
dgbkn <anandrambkn@gmail.com>
2022-03-05 11:51:57 +0530

    asdsad

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 var cors = require("cors");
 var os = require("os");
 const ytdl = require('ytdl-core');
-const puppeteer = require ('puppeteer');
+const puppeteer = require('puppeteer');
 
 
 app.use(cors());
@@ -20,35 +20,50 @@ app.use(cors());
 //       ],});
 //         const page = await browser.newPage();
 //         await page.goto('https://google.com');
-    
+
 //         const pdf = await page.pdf();
 //         return pdf;
 //      }
 
 
-     const cloudloginandrenew = async () => {
-        const browser = await puppeteer.launch({headless: false,args: [
+const cloudloginandrenew = async () => {
+    const browser = await puppeteer.launch({
+        headless: false, args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
-          ],});
-            const page = await browser.newPage();
-            await page.goto('https://google.com');
-        
-            const pdf = await page.pdf();
-            return pdf;
-         }
+        ],
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({width: 1200, height: 720})
+    await page.goto('https://ccp.cloudaccess.net/index.php?rp=/login', { waitUntil: 'networkidle0' });
+    await page.waitForNavigation();
+    await page.type('#inputEmail', "martmast60@gmail.com");
+    await page.type('#inputPassword', "@Anu123456");
+
+  // click and wait for navigation
+  await Promise.all([
+    page.click('#login'),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+  ]);
+
+    const pdf = await page.pdf();
+    return pdf;
+}
 
 
-    app.get('/testa', async function (req, res) {
-        const pdf = await cloudloginandrenew();
-        res.send(pdf);
+app.get('/cloudlogin', async function (req, res) {
+    const pdf = await cloudloginandrenew();
+    res.contentType("application/pdf");
+
+    res.send(pdf);
 });
 
 
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/index.htm"));
-  });
+});
 
 
 app.listen(3000);
